3dbb23990853b59830f9f94270bfdb78718990d7
AdrianC2000 <adrianciesielczyk2000@gmail.com>
2022-09-15 15:33:18 +0200

    IN-20 # Straightening the invoice mechanism implemented

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('test 123')
+from invoice_straightener.invoice_straightener import InvoiceStraightener
+from table_extractor.table_extractor import TableExtractor
+import cv2
+
+
+if __name__ == "__main__":
+    file_path = "resources/censored_invoices/Invoice 1 censored.png"
+    output_straightened_path = "resources/straightened_invoices/Invoice 1 straightened.png"
+    output_extracted_table = "resources/extracted_tabled/Invoice 1 extracted table.png"
+
+    straightened_invoice = InvoiceStraightener(file_path).straighten_image()
+    cv2.imwrite(output_straightened_path, straightened_invoice)
+
+    table = TableExtractor(output_straightened_path).extract_table()
+    print(f"Writing image to {output_extracted_table}")
+    cv2.imwrite(output_extracted_table, table)
